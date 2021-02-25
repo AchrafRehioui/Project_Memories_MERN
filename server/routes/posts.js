cefe47c769121f7fb8cecf789f3a4eb5bf7566e8
Achraf <achraf.rehioui.1@gmail.com>
2021-02-25 17:10:19 +0100

    delete functionality

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,4 +1,4 @@
-const { getPosts, createPost, updatePost } = require('../controllers/posts.js')
+const { getPosts, createPost, updatePost, deletePost } = require('../controllers/posts.js')
 const express = require('express');
 
 const router = express.Router();
@@ -6,5 +6,8 @@ const router = express.Router();
 router.get('/', getPosts);
 router.post('/', createPost);
 router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
+
+
 
 module.exports = router;
