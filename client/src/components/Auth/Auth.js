daf4a42638868c37e74a5b38b660e95b0306e080
Achraf <achraf.rehioui.1@gmail.com>
2021-03-03 19:52:06 +0100

    JWT Login

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -7,6 +7,9 @@ import { useHistory } from 'react-router-dom';
 import Icon from './icon';
 import useStyles from './styles';
 import Input from './Input';
+import { signin, signup } from '../../actions/auth';
+
+const initialState = { firstName: '', lastName: '', email: '', password: '', confirmPassword: '' }
 
 const Auth = () => {
     const classes = useStyles();
@@ -15,18 +18,30 @@ const Auth = () => {
 
     const [isSignup, setIsSignup] = useState(false);
 
+    const [formData, setFormData] = useState(initialState);
+
     const dispatch = useDispatch();
 
     const history = useHistory();
 
     const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword);
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
+        if (isSignup) {
+
+            dispatch(signup(formData, history));
+
+        } else {
+
+            dispatch(signin(formData, history));
+
+        }
     };
 
-    const handleChange = () => {
-
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
     };
 
     const switchMode = () => {
